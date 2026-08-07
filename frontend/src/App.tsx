bd33a210d7f5dbf1eec9d8606ceab5452915a037
Mohammad Mahdi Momeni <mahdimomeni012@gmail.com>
2026-08-07 12:50:03 +0330

    add shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import logo from './assets/images/logo-universal.png';
 import './App.css';
 import {Greet} from "../wailsjs/go/main/App";
 
+import { Button } from "@/components/ui/button"
+
 function App() {
     const [resultText, setResultText] = useState("Please enter your name below 👇");
     const [name, setName] = useState('');
@@ -22,7 +24,7 @@ function App() {
             <div id="result" className="result">{resultText}</div>
             <div id="input" className="input-box">
                 <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
+                <Button variant="outline" onClick={greet}>Greet</Button>
             </div>
         </div>
     )
