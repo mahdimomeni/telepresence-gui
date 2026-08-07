eb629476c22abf1a1c4925b8bc6f20cc19ee0540
Mohammad Mahdi Momeni <mahdimomeni012@gmail.com>
2026-08-07 13:07:41 +0330

    feat: add dark/light mode and it's toggle button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
 import './App.css';
 import {Greet} from "../wailsjs/go/main/App";
 
 import { Button } from "@/components/ui/button"
+import { ThemeProvider } from './components/theme-provider';
+import { ModeToggle } from './components/mode-toggle';
 
 function App() {
     const [resultText, setResultText] = useState("Please enter your name below 👇");
@@ -16,17 +17,19 @@ function App() {
     }
 
     return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <Button variant="outline" onClick={greet}>Greet</Button>
+        <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+            <div id="App">
+                <h1 className="text-3xl font-bold underline">
+                    Hello world!
+                </h1>
+                <div id="result" className="result">{resultText}</div>
+                <div id="input" className="input-box">
+                    <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
+                    <Button variant="outline" onClick={greet}>Greet</Button>
+                </div>
+                <ModeToggle />
             </div>
-        </div>
+        </ThemeProvider>
     )
 }
 
