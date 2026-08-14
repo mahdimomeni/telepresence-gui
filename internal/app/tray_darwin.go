40bb7fcd3bb867b220a3598eff7e9a2c62cb6302
Mohammad Mahdi Momeni <mahdimomeni012@gmail.com>
2026-08-14 23:13:58 +0330

    fix: remove tray support for macos

--- internal/app/tray_darwin.go
+++ internal/app/tray_darwin.go
@@ -1,5 +1,26 @@
+//go:build darwin
+
 package app
 
+import (
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+)
+
+func (a *App) setupSystemTray() {
+
+}
+
 func (a *App) setTrayIcon() {
-	tray.SetIcon(a.darwinTrayIcon)
+}
+
+func (a *App) updateConnectionStatus(connected bool) {
+
+	a.isConnected = connected
+	if connected {
+		_ = a.Notify("Telepresence Connected", "Connected to cluster successfully.")
+	} else {
+		_ = a.Notify("Telepresence Disconnected", "Daemon stopped successfully.")
+	}
+
+	runtime.EventsEmit(a.ctx, "connection-changed", a.isConnected)
 }
--- internal/app/tray_windows.go
+++ internal/app/tray_windows.go
@@ -1,5 +1,91 @@
+//go:build windows
+
 package app
+
+import (
+	"fmt"
+	"telepresence-gui/internal/models"
+
+	"github.com/gogpu/systray"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+)
+
+var mConnectToggle *systray.MenuItem
+var tray *systray.SystemTray
+
+func (a *App) setupSystemTray() {
+	tray = systray.New()
+
+	menu := systray.NewMenu()
+	mConnectToggle = menu.Add("Connect", func() {
+		currentlyConnected := a.isConnected
+
+		if currentlyConnected {
+			runtime.EventsEmit(a.ctx, "connection-pending", true)
+			if err := a.StopTelepresence(); err != nil {
+				_ = a.Notify("Disconnect Failed", fmt.Sprintf("Error: %v", err))
+			} else {
+				runtime.EventsEmit(a.ctx, "connection-pending", false)
+			}
+		} else {
+			config, err := a.configService.LoadConnectConfig()
+			if err != nil || config == nil {
+				config = &models.ConnectConfig{Namespace: "default"}
+			}
+
+			runtime.EventsEmit(a.ctx, "daemon-log", "[Tray] Connecting to cluster...")
+			runtime.EventsEmit(a.ctx, "connection-pending", true)
+			if err := a.StartTelepresence(*config); err != nil {
+				_ = a.Notify("Connection Failed", fmt.Sprintf("Error: %v", err))
+			} else {
+				runtime.EventsEmit(a.ctx, "connection-pending", false)
+			}
+		}
+	})
+	menu.AddSeparator()
+	menu.Add("Quit", func() {
+		_ = a.StopTelepresence()
+		tray.Remove()
+		runtime.Quit(a.ctx)
+	})
+
+	a.setTrayIcon()
+
+	tray.SetTooltip("Telepresence GUI").
+		SetMenu(menu)
+	tray.OnClick(func() {
+		runtime.WindowUnminimise(a.ctx)
+		runtime.WindowShow(a.ctx)
+	})
+	tray.Show()
+
+	go func() {
+		if err := tray.Run(); err != nil {
+			fmt.Println("Tray error:", err)
+		}
+	}()
+}
 
 func (a *App) setTrayIcon() {
 	tray.SetIcon(a.windowsTrayIcon)
+}
+
+func (a *App) updateConnectionStatus(connected bool) {
+	a.statusMu.Lock()
+	a.isConnected = connected
+	a.statusMu.Unlock()
+
+	if connected {
+		_ = a.Notify("Telepresence Connected", "Connected to cluster successfully.")
+		if mConnectToggle != nil {
+			mConnectToggle.SetLabel("Disconnect")
+		}
+	} else {
+		_ = a.Notify("Telepresence Disconnected", "Daemon stopped successfully.")
+		if mConnectToggle != nil {
+			mConnectToggle.SetLabel("Connect")
+		}
+	}
+
+	runtime.EventsEmit(a.ctx, "connection-changed", a.isConnected)
 }
